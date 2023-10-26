7c7544aecd9d24b5e505f624e7f127178875ce8f
Svetlana Trukhina <sweta4ca@mail.ru>
2023-10-27 06:10:36 +0700

    feat: add login, register endpoints, user model

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRETKEY
+
+export default function (req, res, next) {
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+  try{
+    const token = req.headers.authorization.split(' ')[1]
+
+    if (!token) { return res.status(401).json({message:'пользователь не авторизован!'}) }
+
+    const decodeData = jwt.verify(token, SECRET_KEY)
+    req.user = decodeData
+
+    next()
+  }catch (e){
+    return res.status(401).json({message:'пользователь не авторизован!'})
+  }
+}
