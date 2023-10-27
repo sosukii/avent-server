321bf2288fb1ee406895d7f2dfdf8ce7e3d28796
Svetlana Trukhina <sweta4ca@mail.ru>
2023-10-27 08:07:13 +0700

    fix: change port

--- server_instanse.js
+++ server_instanse.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { router as routes } from './routes/routes.js';
 
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use('/', routes);
 
 export async function start() {
   try {
-    app.listen(5000, () => console.log(`♡ server started 5000`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`♡ server started ${PORT}`));
   } catch (e) {
     console.log(e);
   }
